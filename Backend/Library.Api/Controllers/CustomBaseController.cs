f274223412e27aaf8b66873fa0cd8f0cec7be4b1
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-03-01 18:21:17 +0300

    feat(api): added CustomBaseController

---
+++ Backend/Library.Api/Controllers/CustomBaseController.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using Library.Core.Dtos.ResponseDto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CustomActionResult<T>(ResultService<T> result) where T: class
+        {
+            return result.StatusCode switch
+            {
+                HttpStatusCode.NoContent => NoContent(),
+                _ => new ObjectResult(result) { StatusCode = result.StatusCode.GetHashCode() }
+            };
+        }
+
+        [NonAction]
+        public IActionResult CustomActionResult(ResultService result)
+        {
+            return result.StatusCode switch
+            {
+                HttpStatusCode.NoContent => NoContent(),
+                _ => new ObjectResult(result) { StatusCode = result.StatusCode.GetHashCode() }
+            };
+        }
+    }
+}
