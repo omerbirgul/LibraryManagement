46a3223f96fa0f1293df6a4b815f0756f5a4e2ee
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-03-03 15:04:35 +0300

    feat(service): added GetUserClaims function

--- Backend/Library.Service/Services/TokenService.cs
+++ Backend/Library.Service/Services/TokenService.cs
@@ -4,6 +4,7 @@ using Library.Core.Dtos.TokenDtos;
 using Library.Core.Entities;
 using Library.Core.Services;
 using Library.Core.Settings;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
@@ -12,10 +13,26 @@ namespace Library.Service.Services;
 public class TokenService : ITokenService
 {
     private readonly JwtSettings _jwtSettings;
+    private readonly UserManager<AppUser> _userManager;
 
-    public TokenService(IOptions<JwtSettings> jwtSettings)
+    public TokenService(IOptions<JwtSettings> jwtSettings, UserManager<AppUser> userManager)
     {
+        _userManager = userManager;
         _jwtSettings = jwtSettings.Value;
+    }
+
+    private async Task<IEnumerable<Claim>> GetUserClaims(AppUser user)
+    {
+        var userRoles = await _userManager.GetRolesAsync(user);
+        var userClaimList = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+        userClaimList.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+        return userClaimList;
     }
 
     public TokenDto CreateToken(AppUser user)
@@ -23,19 +40,13 @@ public class TokenService : ITokenService
         var securityKey = SignService.GetSymmetricSecurityKey(_jwtSettings.SecretKey);
 
         SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-        var claimsList = new List<Claim>()
-        {
-            new Claim(ClaimTypes.NameIdentifier, user.Id),
-            new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            new Claim(ClaimTypes.Name, user.UserName)
-        };
         
         JwtSecurityToken securityToken = new JwtSecurityToken(
             issuer: _jwtSettings.Issuer,
             audience: _jwtSettings.Audience,
             notBefore: DateTime.Now,
             expires: DateTime.Now.AddMinutes(3),
-            claims: claimsList,
+            claims: GetUserClaims(user).Result,
             signingCredentials: credentials);
 
         JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
