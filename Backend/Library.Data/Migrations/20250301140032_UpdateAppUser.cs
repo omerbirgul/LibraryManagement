77c71940625436beffaed061aef3729b01c2a06a
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-03-04 22:17:32 +0300

    feat: frontend MVC project created

---
+++ Backend/Library.Data/Migrations/20250301140032_UpdateAppUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "AspNetUsers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Frontend/Library.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Library.Frontend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
