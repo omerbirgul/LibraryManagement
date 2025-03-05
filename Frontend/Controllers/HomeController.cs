7781446c731f87594658052a6937c7a15142c5cf
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-03-05 16:39:10 +0300

    feat(MVC): added homepage views

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Library.Mvc.Models;
+using Library.Mvc.Services.BookServices;
 
 namespace Library.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IBookService _bookService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IBookService bookService)
     {
         _logger = logger;
+        _bookService = bookService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var bookResponse = await _bookService.GetAvailableBooks();
+        if(bookResponse is not null && string.IsNullOrEmpty(bookResponse.ErrorMessage))
+        {
+            return View(bookResponse.Data);
+        }
         return View();
     }
 
---
+++ Frontend/Services/JwtServices/JwtHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Library.Mvc.Services.JwtServices
+{
+    public static class JwtHelper
+    {
+        public static string GetClaimValue(string token, string claimType)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+
+            var claim = jwtToken.Claims.FirstOrDefault(x => x.Type == claimType);
+            return claim?.Value ?? "";
+        }
+    }
+}
