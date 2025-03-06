b98b071ea3cd5956ae8a3f67b90c0e224c87dc70
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-03-06 19:39:42 +0300

    feat(mvc): added getting user roles from token

---
+++ Frontend/Services/RoleServices/GetUserRoles.cs
@@ -0,0 +1,21 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Library.Mvc.Services.RoleServices
+{
+    public static class GetUserRoles
+    {
+        public static List<string> GetRolesFromToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(token) as JwtSecurityToken;
+
+            var roles = jsonToken?.Claims
+                .Where(c => c.Type == ClaimTypes.Role)
+                .Select(c => c.Value)
+                .ToList() ?? new List<string>();
+
+            return roles;
+        }
+    }
+}
